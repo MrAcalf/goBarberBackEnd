bbddcb90390125d26822f58a5d05cd3b53b095e9
mracalf <antonio.lima16@fatec.sp.gov.br>
2020-07-13 18:02:16 -0300

    applying dependency inversion and liscov principles

--- src/modules/appointments/infra/typeorm/entities/Appointment.ts
+++ src/modules/appointments/infra/typeorm/entities/Appointment.ts
@@ -1,5 +1,5 @@
 import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, ManyToOne, JoinColumn } from 'typeorm'
-import User from './User'
+import User from '@modules/users/infra/typeorm/entities/User'
 
 @Entity('appointments')
 export default class Appointment {
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,16 +1,19 @@
 import 'reflect-metadata'
 import express, { Request, Response, NextFunction } from 'express'
+import cors from 'cors'
 import 'express-async-errors'
 
 import routes from './routes'
-import uploadConfig from './config/upload'
+import uploadConfig from '@config/upload'
 
-import AppError from './errors/AppError'
+import AppError from '@shared/errors/AppError'
 
-import './database'
+import '@shared/infra/typeorm'
 
 
 const app = express()
+
+app.use(cors())
 app.use(express.json())
 app.use('/files', express.static(uploadConfig.directory))
 app.use(routes)
