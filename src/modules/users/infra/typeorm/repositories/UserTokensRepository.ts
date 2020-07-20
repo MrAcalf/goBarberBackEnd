c68b4aa24327bd18f953868075beca72105c889e
mracalf <antonio.lima16@fatec.sp.gov.br>
2020-07-20 18:27:28 -0300

    TypeORM migration createsd and UserTokensRepository and Interface are injected on the shared container Index.ts

---
+++ src/modules/users/infra/typeorm/repositories/UserTokensRepository.ts
@@ -0,0 +1,30 @@
+import IUsersTokensRepository from '@modules/users/repositories/IUserTokensRepository'
+import { getRepository, Repository } from 'typeorm'
+
+import UserToken from '../entities/UserToken'
+
+export default class UsersTokensRepository implements IUsersTokensRepository {
+    private ormRepository: Repository<UserToken>
+
+    constructor() {
+        this.ormRepository = getRepository(UserToken)
+    }
+
+    public async findByToken(token: string): Promise<UserToken | undefined> {
+        const userToken = await this.ormRepository.findOne({
+            where: { token }
+        })
+
+        return userToken
+    }
+
+    public async generate(user_id: string): Promise<UserToken> {
+        const userToken = this.ormRepository.create({
+            user_id
+        })
+
+        await this.ormRepository.save(userToken)
+
+        return userToken
+    }
+}
